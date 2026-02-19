77e8a3f9949aae921b5017806baaa223e2563a52
felvaldes <valdesfeli11@gmail.com>
2026-02-19 15:33:25 -0300

    Test application

--- src/test/java/com/javaproject/backend/BackendApplicationTests.java
+++ src/test/java/com/javaproject/backend/BackendApplicationTests.java
@@ -1,13 +1,24 @@
 package com.javaproject.backend;
 
+import org.hibernate.annotations.TimeZoneStorage;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.javaproject.backend.repository.RepoCategory;
+import org.springframework.test.context.ActiveProfiles;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	RepoCategory repoCategory;
 
+	@Test
+	void contextLoads(){
+		assertNotNull(repoCategory);
+	}
 }
